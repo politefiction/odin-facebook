1e59f6767529318fff31de0c7dde379f45b1ddbb
Chelsi Jones <cajones.bz@gmail.com>
2018-05-21 09:55:18 -0500

    Revised Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root 'users#index'
-
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  devise_scope :user do
-    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  end
+  get 'users/show', as: "profile"
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,23 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  # So, could use a profile test or two here; would need to 
+  # add some fixtures for that
+  # Probably also index test, if that's my front page
+
+end
+
+# Not sure I need all this; storing here in case
+
+=begin
+  test "should get index as root" do
+    get root_url
+    assert_template :index 
+  end
+
   test "should get new" do
-    get users_new_url
+    get new_user_registration_path
     assert_response :success
   end
 
@@ -15,5 +30,4 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get users_edit_url
     assert_response :success
   end
-
-end
+=end
