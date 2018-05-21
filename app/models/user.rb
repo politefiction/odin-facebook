ef6c2126a77a62fc1c1a3d2a8448d48d2c0887cc
Chelsi Jones <cajones.bz@gmail.com>
2018-05-21 09:56:31 -0500

    Add some User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable, 
          :omniauthable, omniauth_providers: %i[facebook]
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
-      user.name = auth.info.name
+      user.first_name = auth.info.first_name
+      user.last_name = auth.info.last_name
       # If you add an image to the User model
       # user.image = auth.info.image
     end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @example = users(:example)
+  end
+
+  test "first name should be present" do
+    @example.first_name = nil
+    assert_not @example.save
+  end
+
+  test "last name should be present" do
+    @example.last_name = nil
+    assert_not @example.save
+  end
+
 end
