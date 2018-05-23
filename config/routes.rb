8cdfca6333691d89b6c4835c7528acadbf51e5a5
Chelsi Jones <cajones.bz@gmail.com>
2018-05-23 07:52:09 -0500

    Add basic user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'users#index'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  get 'users/show', as: "profile"
+  get 'profile/:id', to: 'users#show', as: 'profile'
 
 
 
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,33 +1,16 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
 
-  # So, could use a profile test or two here; would need to 
-  # add some fixtures for that
-  # Probably also index test, if that's my front page
+  def setup
+    @user = users(:example)
+  end
+
+  test "should get profile" do
+    sign_in @user
+    get profile_path(@user)
+    assert_response :success
+  end
 
 end
-
-# Not sure I need all this; storing here in case
-
-=begin
-  test "should get index as root" do
-    get root_url
-    assert_template :index 
-  end
-
-  test "should get new" do
-    get new_user_registration_path
-    assert_response :success
-  end
-
-  test "should get show" do
-    get users_show_url
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get users_edit_url
-    assert_response :success
-  end
-=end
