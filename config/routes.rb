1d29ce1d6ef702e5ac92f7ccfff0a13da5b618c6
Chelsi Jones <cajones.bz@gmail.com>
2018-06-03 08:45:02 -0500

    Add internal messaging

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#index'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  resources :conversations do
+    resources :messages, only: [:index, :create]
+    get 'messages', on: :member
+  end
+
   get 'profile/:id', to: 'users#show', as: 'profile'
-
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
 
 end
