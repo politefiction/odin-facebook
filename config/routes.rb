73dbe419bb0d9632f6d425864ea5b14dc8c0a8fa
Chelsi Jones <cajones.bz@gmail.com>
2018-06-28 08:24:19 -0500

    Add user list

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
+  root 'static_pages#home'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   resources :conversations do
@@ -10,6 +12,7 @@ Rails.application.routes.draw do
   resources :friend_requests
   resources :friendships, only: [:create, :destroy]
 
+  get 'users', to: 'users#index'
   get 'profile/:id', to: 'users#show', as: 'profile'
   get 'friends', to: 'users#friends_list', as: 'friends'
 end
