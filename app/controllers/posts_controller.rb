eb104baf1a0ed4ca9e8e22eae52b1eb51f52df25
Chelsi Jones <cajones.bz@gmail.com>
2018-06-29 09:31:01 -0500

    Set up Posts controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,51 @@
+class PostsController < ApplicationController
+  def index
+    @posts = User.find(params[:user_id]).posts
+  end
+
+  def new
+    @user = current_user
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:success] = "Post created."
+      redirect_to user_posts_path(current_user)
+    else
+      flash[:alert] = "Hmm, something went wrong. Try again?"
+      redirect_to new_user_post_path
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+    @user = @post.user
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      flash[:success] = "Post updated."
+      redirect_to user_post_path(@post.user, @post)
+    else
+      flash[:alert] = "Hmm, something went wrong. Try again?"
+      edit_user_post(@post)
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+  resources :users do
+    resources :posts
+  end
 
   resources :conversations do
     resources :messages, only: [:index, :create]
@@ -11,6 +13,7 @@ Rails.application.routes.draw do
   end
   resources :friend_requests
   resources :friendships, only: [:create, :destroy]
+  #resources :posts
 
   get 'users', to: 'users#index'
   get 'profile/:id', to: 'users#show', as: 'profile'
