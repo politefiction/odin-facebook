d1cb28f95091b78f7daa9fb55a294b2fdfd4a9f7
Chelsi Jones <cajones.bz@gmail.com>
2018-07-01 11:57:59 -0500

    Create basic post tests

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,19 +1,44 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get posts_index_url
-    assert_response :success
+  def setup
+    @grace = users(:grace)
+    @marianna = users(:marianna)
+    @sid = users(:sid)
   end
 
-  test "should get new" do
-    get posts_new_url
-    assert_response :success
+  test "user can make new posts" do
+    sign_in @grace
+    get new_user_post_path(@grace)
+    assert_difference "Post.count" do
+      post user_posts_path, params: {
+        post: {
+          title: "Test Post", 
+          body: "The quick, brown fox jumps over a lazy dog."
+        }
+      }
+    end
   end
 
-  test "should get edit" do
-    get posts_edit_url
+  test "posts viewable only by user or user's friends" do
+    Friendship.create(friend_id: @grace.id, inverse_friend_id: @marianna.id)
+    Friendship.create(friend_id: @marianna.id, inverse_friend_id: @grace.id)
+    assert @grace.friends.include? @marianna
+    assert_not @grace.friends.include? @sid
+
+    post = Post.create(user_id: @grace, title: "Test Post", body: "The quick, brown fox jumps over a lazy dog.")
+
+    sign_in @grace
+    get user_posts_path(@grace)
     assert_response :success
+
+    sign_in @marianna
+    get user_posts_path(@grace)
+    assert_response :success
+    
+    sign_in @sid
+    get user_posts_path(@grace)
+    assert_redirected_to root_url
   end
 
 end
