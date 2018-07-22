2bc850ea060fb4fb4089a7149b48a0b01d6b1c34
Chelsi Jones <cajones.bz@gmail.com>
2018-07-22 08:18:36 -0500

    Set up basic controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'comments/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
