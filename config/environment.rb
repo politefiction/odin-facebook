b6816a8a96f577e3fc9f9d24cf2cc80ff13ca2c8
Chelsi Jones <cajones.bz@gmail.com>
2018-08-28 19:32:31 -0500

    More SendGrid

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,10 @@ ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
   :domain => 'pacific-thicket-49521.herokuapp.com',
+  :host => 'pacific-thicket-49521.herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true
 }
+
