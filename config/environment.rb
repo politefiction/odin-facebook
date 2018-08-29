addd68d9ed7550228c35792a369771e0f8d7aea0
Chelsi Jones <cajones.bz@gmail.com>
2018-08-28 19:41:58 -0500

    Yet more SendGrid

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,6 @@
 # Load the Rails application.
 require_relative 'application'
 
-# Initialize the Rails application.
-Rails.application.initialize!
-
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
@@ -14,4 +11,9 @@ ActionMailer::Base.smtp_settings = {
   :authentication => :plain,
   :enable_starttls_auto => true
 }
+
+# Initialize the Rails application.
+Rails.application.initialize!
+
+
 
