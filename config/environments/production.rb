bb4ce56caae9a7edce6fa47dd4fb847b6fb43c57
Chelsi Jones <cajones.bz@gmail.com>
2018-08-29 06:07:47 -0500

    Maybe this time

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
 
   # ActionMailer settings for SendGrid
-  config.action_mailer.default_options = { host: 'https://pacific-thicket-49521.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'pacific-thicket-49521.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
